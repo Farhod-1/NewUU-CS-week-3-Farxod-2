e721fa3208f4ac4ff4b7d5fa47e19baba64afc35
Farhod-1 <93403853+Farhod-1@users.noreply.github.com>
2023-04-05 13:48:40 +0500

    Update problem 2.cpp

--- problem 2.cpp
+++ problem 2.cpp
@@ -1,11 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int n, m;
-    cin>> n >> m;
-    cout<< "The result of sum is equal to " << n + m << endl;
-    cout<< "The result of product is equal to " << n * m << endl;
-
-
+    float n;
+    float p = 3.14;
+    cin>> n;
+    cout << "The diameter of circle is equal to " << 2 * n << endl;
+    cout << "The circumference of circle is equal to " << 2 * p * n << endl;
+    cout << "The area of circle is equal to " << p * n * n << endl;
+    return 0;
     }
