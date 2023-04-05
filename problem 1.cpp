954938d08185360e5741acda879d069f69e7469b
Farhod-1 <93403853+Farhod-1@users.noreply.github.com>
2023-04-05 13:47:54 +0500

    Update problem 1.cpp

--- problem 1.cpp
+++ problem 1.cpp
@@ -1,3 +1,11 @@
-//
-// Created by farho on 4/5/2023.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin>> n >> m;
+    cout<< "The result of sum is equal to " <<  n + m << endl;
+    cout<< "The result of product is equal to " << n * m << endl;
+
+
+    }
