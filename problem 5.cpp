8a5b9901165b47d7cda0f37ce0a9245a1d70ff48
Farhod-1 <93403853+Farhod-1@users.noreply.github.com>
2023-04-05 13:51:11 +0500

    Update problem 5.cpp

--- problem 5.cpp
+++ problem 5.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
     int n, m;
     cin>> n >> m;
-    cout<< "The result of sum is equal to " << n + m << endl;
-    cout<< "The result of product is equal to " << n * m << endl;
-
-
-    }
+    if(n >= m){
+        cout << 1;
+    }else  cout << 0;
+    return 0;
+}
